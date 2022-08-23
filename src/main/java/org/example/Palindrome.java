eb95fd0e85076a504eb2ab5b6fbdcb3f9c2187f7
Bram Janssens <bram.janssens@infosupport.com>
2022-08-23 16:48:37 +0200

    Initial.

---
+++ src/main/java/org/example/Palindrome.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Palindrome {
+
+    public boolean isPalindrome(String s) {
+        return true;
+    }
+}
---
+++ src/test/java/org/example/PalindromeTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+class PalindromeTest {
+
+    @Test
+    void isPalindrome() {
+
+    }
+}
